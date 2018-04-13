f63eeb74e0523b18c19a43f08876351ce89bdcfe
Mark Parnwell <markparnwell@itsstonbury.co.uk>
2018-04-13 15:29:29 +0100

    0.1.7 #12 Namespace model collections in Vuex store under a single Vuex module Namespace configurable but defaults to clientORM

--- stores/vuex.js
+++ stores/vuex.js
@@ -1,49 +1,66 @@
-module.exports = function (store) {
-  var storage = {};
-  storage.store = store;
-  storage.get = function (store, name) {
-    if (!store.state[name]){
-      store.registerModule(name, {
-        namespaced : true,
-        state : {
-          data : []
-        },
-        mutations : {
-          CREATE : function(state, payload){
-            state.data.push(payload.data);
-            return state.data[state.data.length-1];
-          },
-          UPDATE : function(state, payload){
-            var index = payload.index;
-            var existing = state.data[index];
-            Object.assign(existing, payload.data);
-            state.data.splice(index, 1, existing);
-            return existing;
-          },
-          DELETE : function(state, payload){
-            state.data.splice(payload.index, 1);
-          }
-        }
-      });
+const defaultNamespace = 'clientORM';
+
+function newVuexModule() {
+  return {
+    namespaced: true,
+    state: {
+      data: []
+    },
+    mutations: {
+      CREATE(state, payload) {
+        state.data.push(payload.data);
+        return state.data[state.data.length - 1];
+      },
+      UPDATE(state, payload) {
+        const index = payload.index;
+        const existing = state.data[index];
+        Object.assign(existing, payload.data);
+        state.data.splice(index, 1, existing);
+        return existing;
+      },
+      DELETE(state, payload) {
+        state.data.splice(payload.index, 1);
+      }
     }
-    return store.state[name].data;
+  }
+}
+
+module.exports = function(store, namespace) {
+  namespace = namespace || defaultNamespace;
+
+  // Create client-orm module under models namespace
+  if (!store.state[namespace]) {
+    store.registerModule(namespace, {
+      namespaced: true,
+      state: {}
+    });
+  }
+
+  return {
+    store,
+    get(store, name) {
+      if (!store.state[namespace][name]) {
+        store.registerModule([namespace, name], newVuexModule());
+      }
+      return store.state[namespace][name].data;
+    },
+    create(store, name, payload) {
+      store.commit(`${namespace}/${name}/CREATE`, { data: payload });
+      const data = this.get(store, name);
+      return data[data.length - 1];
+    },
+    update(store, name, payload, idField) {
+      const index = this.get(store, name)
+        .map(row => row[idField])
+        .indexOf(payload[idField]);
+      store.commit(`${namespace}/${name}/UPDATE`, { index, data: payload });
+      return this.get(store, name)[index];
+    },
+    delete(store, name, payload, idField) {
+      const index = this.get(store, name)
+        .map(row => row[idField])
+        .indexOf(payload[idField]);
+      store.commit(`${namespace}/${name}/DELETE`, { index });
+    }
   };
-  storage.create = function (store, name, payload) {
-    store.commit(name + '/CREATE', {data : payload});
-    return store.state[name].data[store.state[name].data.length-1];
-  };
-  storage.update = function (store, name, payload, idField) {
-    var index = store.state[name].data.map(function(row){
-      return row[idField];
-    }).indexOf(payload[idField]);
-    store.commit(name + '/UPDATE', {index : index, data : payload});
-    return store.state[name].data[index];
-  };
-  storage.delete = function (store, name, payload, idField) {
-    var index = store.state[name].data.map(function(row){
-      return row[idField];
-    }).indexOf(payload[idField]);
-    store.commit(name + '/DELETE', {index : index});
-  };
-  return storage;
 };
